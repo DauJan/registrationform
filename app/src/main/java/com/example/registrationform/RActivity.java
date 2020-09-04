2d250ea999825d2eae0a8f7fd7fc1ac12ddfadfa
DauJan <dauzhan02@gmail.com>
2020-09-04 12:33:53 +0300

    'first'

--- app/src/main/java/com/example/registrationform/RActivity.java
+++ app/src/main/java/com/example/registrationform/RActivity.java
@@ -2,16 +2,37 @@ package com.example.registrationform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RActivity extends AppCompatActivity {
-
-
+    ImageView imageView;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_r);
+
+        imageView.findViewById(R.id.imageView2);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RActivity.this,MainActivity.class);
+                finish();
+            }
+        });
+        textView.findViewById(R.id.in);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RActivity.this,MainActivity.class);
+                finish();
+            }
+        });
 
     }
 }
